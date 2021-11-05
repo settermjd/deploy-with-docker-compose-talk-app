30c6343ddc3f67af6435ddcb31fe08cbd767bbcd
Matthew Setter <matthew@matthewsetter.com>
2021-11-05 16:27:32 +0100

    First commit

---
+++ config/autoload/dependencies.global.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'dependencies' => [
+        // Use 'aliases' to alias a service name to another service. The
+        // key is the alias name, the value is the service to which it points.
+        'aliases' => [
+        ],
+        // Use 'invokables' for constructor-less services, or services that do
+        // not require arguments to the constructor. Map a service name to the
+        // class name.
+        'invokables' => [
+        ],
+        // Use 'factories' for services provided by callbacks/factory classes.
+        'factories'  => [
+        ],
+    ],
+];
---
+++ src/App/src/Handler/HomePageHandler.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
+use Mezzio\LaminasView\LaminasViewRenderer;
+use Mezzio\Plates\PlatesRenderer;
+use Mezzio\Router;
+use Mezzio\Template\TemplateRendererInterface;
+use Mezzio\Twig\TwigRenderer;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class HomePageHandler implements RequestHandlerInterface
+{
+    /** @var string */
+    private $containerName;
+
+    /** @var Router\RouterInterface */
+    private $router;
+
+    /** @var null|TemplateRendererInterface */
+    private $template;
+
+    public function __construct(
+        string $containerName,
+        Router\RouterInterface $router,
+        ?TemplateRendererInterface $template = null
+    ) {
+        $this->containerName = $containerName;
+        $this->router        = $router;
+        $this->template      = $template;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        $data = [];
+
+        $dbh = new \PDO(
+            'mysql:host=database;port=3306;dbname=project_db',
+            'user',
+            'password'
+        );
+        $stmt = $dbh->query('SELECT * FROM records ORDER BY last_name');
+
+        return new HtmlResponse(
+            $this->template->render(
+                'app::home-page',
+                [ 'stmt' => $stmt ]
+            )
+        );
+    }
+}
