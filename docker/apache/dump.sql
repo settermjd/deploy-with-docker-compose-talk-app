e2ad8aaee4d9bc83021a570450eee7ca6b2fdf4f
Matthew Setter <matthew@matthewsetter.com>
2023-05-26 12:54:02 +0200

    Rework the Docker configuration
    The configuration contained a lot of outdated files which needed to be
    updated, unused files which which needed to be removed. This change
    handles both of these issues. It also simplifies the configuration by
    using Apache with PHP in one container instead of PHP and NGINX in two
    separate containers.

--- docker/apache/dump.sql
+++ docker/apache/dump.sql
@@ -1,24 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.27, for macos11.6 (x86_64)
---
--- Host: localhost    Database: project_db
--- ------------------------------------------------------
--- Server version	8.0.27
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `actors`
---
-
 DROP TABLE IF EXISTS `actors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
